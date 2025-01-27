d31ae7c660212297643ec43463a69c0d3a6b0ebb
NatthasithBoomheng <0949743436mix@gmail.com>
2025-01-27 17:24:03 +0700

    getOwnPlaylistFunction

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import connection from './config/database.js';
+import playlistRoute from './routes/playlistRoute.js';
 
 const app = express();
 const port = 5555;
 
 app.use(express.json());
+
+app.use("/api/playlists", playlistRoute);
 
 connection.connect((err)=>{
     if (err){console.log(err);} else{console.log("Database connected");}
